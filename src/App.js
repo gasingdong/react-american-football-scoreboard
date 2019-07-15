be3577f2be87bce6dcd4e72f847c716774b607f2
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-15 12:48:10 -0700

    change hardcoded values to dynamic values

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import {React, useState} from "react";
+import React, {useState} from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -17,12 +17,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{homeScore}</div>
           </div>
           <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{awareScore}</div>
           </div>
         </div>
         <BottomRow />
