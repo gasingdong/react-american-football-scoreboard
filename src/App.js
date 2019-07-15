dbc5f5729f200db8ef3a185c6f606dabc6756ad7
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-15 13:16:18 -0700

    add teamscore component

--- src/App.js
+++ src/App.js
@@ -6,26 +6,20 @@ import BottomRow from "./BottomRow";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
-  const [awareScore, setAwayScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
   const touchdown = 7;
   const fieldGoal = 3;
+
+  const homeTeam = 'Lions';
+  const awayTeam = 'Tigers';
 
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
-          <div className="home">
-            <h2 className="home__name">Lions</h2>
-
-            {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
-
-            <div className="home__score">{homeScore}</div>
-          </div>
+          <TeamScore home={true} teamName="Lions" score={homeScore}/>  
           <div className="timer">00:03</div>
-          <div className="away">
-            <h2 className="away__name">Tigers</h2>
-            <div className="away__score">{awareScore}</div>
-          </div>
+          <TeamScore teamName="Tigers" score={awayScore}/>
         </div>
         <BottomRow />
       </section>
@@ -36,10 +30,20 @@ function App() {
           <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + fieldGoal)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => setAwayScore(awareScore + touchdown)}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awareScore + fieldGoal)}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + touchdown)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => setAwayScore(awayScore + fieldGoal)}>Away Field Goal</button>
         </div>
       </section>
+    </div>
+  );
+}
+
+function TeamScore(prop) {
+  const prefix = prop.home ? "home" : "away";
+  return (
+    <div className={prefix}>
+      <h2 className={prefix + "__name"}>{prop.teamName}</h2>
+      <div className={prefix + "__score"}>{prop.score}</div>
     </div>
   );
 }
